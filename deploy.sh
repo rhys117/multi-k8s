18364bec0b588e7cd17e16a1fa9440bb47f6bb8b
rhys117 <rhys117@gmail.com>
2019-09-06 22:54:19 +1000

    fix deploy sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-docker build -t stephengrider/multi-client:latest -t rhys117/multi-client:$SHA  -f ./client/Dockerfile ./client
-docker build -t stephangrider/multi-server -t rhys117/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t stephangrider/multi-worker -t rhys117/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t rhys117/multi-client:latest -t rhys117/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t rhys117/multi-server:latest -t rhys117/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t rhys117/multi-worker:latest -t rhys117/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push rhys117/multi-client:latest
 docker push rhys117/multi-server:latest
@@ -14,5 +14,5 @@ docker push rhys117/multi-worker:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=rhys117/multi-server:$SHA
-kubectl set image deployments/client-deployment server=rhys117/multi-client:$SHA
-kubectl set image deployments/worker-deployment server=rhys117/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=rhys117/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=rhys117/multi-worker:$SHA
